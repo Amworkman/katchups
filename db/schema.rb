c832d62eac9a0b50ea120b91510f2ed4fda4c5a4
Adam Workman <adammworkman25@gmail.com>
2020-11-25 14:31:47 -0500

    add image and menue to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_174442) do
+ActiveRecord::Schema.define(version: 2020_11_25_190453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_174442) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img", default: "No image available"
+    t.string "menue", default: "No menue available"
   end
 
   create_table "users", force: :cascade do |t|
