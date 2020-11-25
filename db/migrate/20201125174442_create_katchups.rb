dfcdba392b6d8e44b09838988eeb95330b3dd969
Adam Workman <adammworkman25@gmail.com>
2020-11-25 13:11:56 -0500

    typo fix references

--- db/migrate/20201125174442_create_katchups.rb
+++ db/migrate/20201125174442_create_katchups.rb
@@ -1,7 +1,7 @@
 class CreateKatchups < ActiveRecord::Migration[6.0]
   def change
     create_table :katchups do |t|
-      t.referances :relationship, null: false, foreign_key: true
+      t.references :relationship, null: false, foreign_key: true
       t.references :restaurant, null: false, foreign_key: true
       t.datetime :starts_at
       t.boolean :confirmed, default: :false
