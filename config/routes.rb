c71068b378c08e8ff5853eda644ae5689b509c2d
Adam Workman <adammworkman25@gmail.com>
2020-12-03 12:24:08 -0500

    login, auto_login, pending_friends, friends, dlt_p

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   resources :katchups
   resources :relationships
   resources :users
+
+  post "/login", to:"users#login"
+  get "/auto_login", to: "users#auto_login"
+  get "/pending_friends", to: "users#pending_friends"
+  get "/friends", to: "users#friends"
+  delete "/delete_pending", to: "relationships#delete_pending"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
