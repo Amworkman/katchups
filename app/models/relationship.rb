b32dd7abc95de5df0a1b0aa6f7f80ba3583b7955
Adam Workman <adammworkman25@gmail.com>
2020-12-03 12:22:27 -0500

    def relationship methods

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,29 @@
 class Relationship < ApplicationRecord
   belongs_to :user
-  has many :katchups
-  has_many :restaurants, theough: :katchups
+  has_many :katchups
+
+  def self.reacted?(id1, id2)
+    case1 = !Relationship.where(user_id: id1, friend_id: id2).empty?
+    case2 = !Relationship.where(user_id: id2, friend_id: id1).empty?
+    case1 || case2
+  end
+
+  def self.confirmed_record?(id1, id2)
+    case1 = !Relationship.where(user_id: id1, friend_id: id2, confirmed: true).empty?
+    case2 = !Relationship.where(user_id: id2, friend_id: id1, confirmed: true).empty?
+    case1 || case2
+  end
+
+  def self.find_relationship(id1, id2)
+    if Relationship.where(user_id: id1, friend_id: id2, confirmed: true).empty?
+      Relationship.where(user_id: id2, friend_id: id1, confirmed: true)[0].id
+    else
+      Relationship.where(user_id: id1, friend_id: id2, confirmed: true).id
+    end
+  end
+
+  def self.pending_requests (id1, id2)
+    Relationship.where(user_id: id1, friend_id: id2, confirmed: false)
+  end
+
 end
