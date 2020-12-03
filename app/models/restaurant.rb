fabfb27c08ab1ae507d1b7c70e986cbe3a5f938e
Adam Workman <adammworkman25@gmail.com>
2020-12-03 12:23:02 -0500

    yelp api access

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,21 @@
-class Restaurant < ApplicationRecord
-    has_many :katchups
-    has_many :relationship, through: :katchups 
+class Restaurant < ApplicationRecord    
+
+    @@api_key = "#{ENV['YELP_API_KEY']}"
+
+    @@headers = {
+        headers: {
+            "Authorization" => "Bearer #{@@api_key}"
+            }
+        }
+
+    def self.location_restaurants(location, search_terms="")
+        @search_url = "https://api.yelp.com/v3/businesses/search?location=#{location}&category=restaurants&term=#{search_terms}&sort_by=best_match"
+        HTTParty.get(@search_url, @@headers)
+    end
+
+    def self.single(restaurant_id)
+        @details_url = "https://api.yelp.com/v3/businesses/#{restaurant_id}"
+        HTTParty.get(@details_url, @@headers)
+    end
+
 end
