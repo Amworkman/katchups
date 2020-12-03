06182ecbd976bd0215f35717e5e94fce055c796a
Adam Workman <adammworkman25@gmail.com>
2020-12-03 12:20:17 -0500

    set restaurant, set location

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,51 +1,29 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :update, :destroy]
+  before_action :set_restaurant, only: [:show]
+  before_action :set_location, only:[:index]
 
   # GET /restaurants
   def index
-    @restaurants = Restaurant.all
-
+    @restaurants = Restaurant.location_restaurants(@location, @search_terms="")
+    logged_in_user
     render json: @restaurants
   end
 
   # GET /restaurants/1
   def show
+    
     render json: @restaurant
-  end
-
-  # POST /restaurants
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-
-    if @restaurant.save
-      render json: @restaurant, status: :created, location: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /restaurants/1
-  def update
-    if @restaurant.update(restaurant_params)
-      render json: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /restaurants/1
-  def destroy
-    @restaurant.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
+      #@restaurant = Restaurant.single("E8RJkjfdcwgtyoPMjQ_Olg")
+      @restaurant = Restaurant.single(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone_number, :description, :rating, :cuisine)
+    def set_location
+      @location = "Ellijay GA"
+      #@location = current_user.location
     end
 end
