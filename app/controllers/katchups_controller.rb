991c5ee2d5b9f80bf3fcf8d01c4881be9418ba0b
Adam Workman <adammworkman25@gmail.com>
2020-12-06 15:58:44 -0500

    clean up

--- app/controllers/katchups_controller.rb
+++ app/controllers/katchups_controller.rb
@@ -1,19 +1,16 @@
 class KatchupsController < ApplicationController
   before_action :set_katchup, only: [:show, :update, :destroy]
-
-  # GET /katchups
+  
   def index
     @katchups = Katchup.all
 
     render json: @katchups
   end
-
-  # GET /katchups/1
+  
   def show
     render json: @katchup
   end
-
-  # POST /katchups
+  
   def create
     @katchup = Katchup.new(katchup_params)
 
@@ -23,8 +20,7 @@ class KatchupsController < ApplicationController
       render json: @katchup.errors, status: :unprocessable_entity
     end
   end
-
-  # PATCH/PUT /katchups/1
+  
   def update
     if @katchup.update(katchup_params)
       render json: @katchup
@@ -32,19 +28,17 @@ class KatchupsController < ApplicationController
       render json: @katchup.errors, status: :unprocessable_entity
     end
   end
-
-  # DELETE /katchups/1
+  
   def destroy
     @katchup.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_katchup
       @katchup = Katchup.find(params[:id])
     end
-
-    # Only allow a trusted parameter "white list" through.
+    
     def katchup_params
       params.require(:katchup).permit(:relationsship, :restaurant_id, :starts_at)
     end
