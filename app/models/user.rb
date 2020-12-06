47275e7e58e5918d6e914212fdd43312304c141c
Adam Workman <adammworkman25@gmail.com>
2020-12-06 16:00:05 -0500

    pending firends bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,15 @@ class User < ApplicationRecord
     has_many :pending_relationships, -> { where confirmed: false }, class_name: 'Relationship', foreign_key: "friend_id"
 
     def friends
-        friends_sent_invitation = Relationship.where(user_id: id, confirmed: true).pluck(:friend_id)
-        friends_recieved_invitation = Relationship.where(friend_id: id, confirmed: true).pluck(:user_id)
-        ids = friends_sent_invitation + friends_recieved_invitation
+        sent_invitation = Relationship.where(user_id: id, confirmed: true).pluck(:friend_id)
+        recieved_invitation = Relationship.where(friend_id: id, confirmed: true).pluck(:user_id)
+        ids = sent_invitation + recieved_invitation
         User.where(id: ids)
     end
 
     def pending_friends
-        friends_sent_invitation = Relationship.where(user_id: id, confirmed: false).pluck(:friend_id)
-        friends_recieved_invitation = Relationship.where(friend_id: id, confirmed: false).pluck(:user_id)
-        ids = friends_sent_invitation + friends_recieved_invitation
-        User.where(id: ids)
+        recieved_invitation = Relationship.where(friend_id: id, confirmed: false).pluck(:user_id)
+        User.where(id: recieved_invitation)
     end
 
     def friend_with?(user)
