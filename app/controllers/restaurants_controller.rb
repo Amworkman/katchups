81829dd87c4dad47d587914f1cb7fb93254b6f55
Adam Workman <adammworkman25@gmail.com>
2020-12-06 15:59:23 -0500

    clean up

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,28 +2,25 @@ class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show]
   before_action :set_location, only:[:index]
 
-  # GET /restaurants
+  
   def index
     @restaurants = Restaurant.location_restaurants(@location, @search_terms="")
     logged_in_user
     render json: @restaurants
   end
 
-  # GET /restaurants/1
-  def show
-    
+  
+  def show    
     render json: @restaurant
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      #@restaurant = Restaurant.single("E8RJkjfdcwgtyoPMjQ_Olg")
+    
+    def set_restaurant      
       @restaurant = Restaurant.single(params[:id])
     end
 
     def set_location
-      @location = "Ellijay GA"
-      #@location = current_user.location
+      @location = "Ellijay GA"      
     end
 end
