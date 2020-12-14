1d035dd9ea598fa4e0ab2ffae02da0d98ce3f2f0
Adam Workman <adammworkman25@gmail.com>
2020-12-13 19:18:37 -0500

    refactor

--- app/controllers/katchups_controller.rb
+++ app/controllers/katchups_controller.rb
@@ -22,23 +22,17 @@ class KatchupsController < ApplicationController
     end
   end
   
-  def update
-    @relationship = Relationship.find_relationship(params["user_id"], params["friend_id"])
-    @katchup = Katchup.where(relationship_id: @relationship)
-     
-    if params[:user_restaurant]
-      @katchup.first.user_array << params[:user_restaurant]
-    elsif params[:friend_restaurant]
-      @katchup.first.friend_array << params[:friend_restaurant]
-    end
-
-    if @katchup.first.update(katchup_params)
-      render json: @katchup
-    else
-      render json: @katchup.errors, status: :unprocessable_entity
+  def update     
+    if params[:id]    
+      @katchup = Katchup.find_by_id(params[:id]) 
+      if @katchup.update(katchup_params)
+        render json: @katchup
+      else 
+        render json: @katchup.errors, status: :unprocessable_entity
+      end     
     end
   end
-  
+    
   def destroy
     @katchup.destroy
   end
@@ -50,6 +44,6 @@ class KatchupsController < ApplicationController
     end
     
     def katchup_params
-      params.permit(:relationsship, :restaurant_id,:friend_id, :user_id, :user_array, :friend_array, :user_confimation, :friend_confimation, :starts_at, :location)
+      params.permit(:relationsship, :restaurant_id, :user_confirmation, :friend_confirmation, :starts_at, :user_id, :friend_id,  :location, :user_array => [], :friend_array => [])
     end
 end
