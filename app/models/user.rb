c2ec42422c2692e0fce82a08458edb79e56bd778
Adam Workman <adammworkman25@gmail.com>
2021-04-03 02:02:44 -0400

    Attatch base64 image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,24 @@
-class User < ApplicationRecord 
+require 'open-uri'
+
+class User < ApplicationRecord
+    include ActiveStorageSupport::SupportForBase64
     include Rails.application.routes.url_helpers   
     has_secure_password
     has_many :relationships
     has_many :pending_relationships, -> { where confirmed: false }, class_name: 'Relationship', foreign_key: "friend_id"
 
-    has_one_attached :profile_img
+    has_one_base64_attached :profile_img
+    # before_update :grab_image
+    
+
+    def grab_image     
+        decoded_image = Base64.decode64(self.profile_img_url.split(',')[1])
+        self.profile_img.attach(io: decoded_image, content_type: 'image/jpeg', filename: 'image.jpg')
+    end
     
     def get_image_url
         url_for(self.profile_img)
-      end
+    end
 
     def friends
         sent_invitation = Relationship.where(user_id: id, confirmed: true).pluck(:friend_id)
