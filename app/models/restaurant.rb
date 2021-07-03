f6ccdf266ce9dce03bd4213a62055f96d96b5410
Adam Workman <adammworkman25@gmail.com>
2021-07-03 01:20:43 -0400

    Refactor location_restauransts for graphql

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,16 +1,50 @@
 class Restaurant < ApplicationRecord    
 
-    @@api_key = "#{ENV['YELP_API_KEY']}"
+    @@api_key = "#{ENV['YELP_API_KEY']}" 
+    @@url = "https://api.yelp.com/v3/graphql"  
 
     @@headers = {
         headers: {
-            "Authorization" => "Bearer #{@@api_key}"
+            "Authorization" => "Bearer #{@@api_key}",
         }
     }
 
+    @@fragment = <<-GRAPHQL
+        business {
+            id
+            name
+            price
+            rating
+            location {
+                formatted_address
+            }
+            categories {
+                title
+            }
+            photos
+            display_phone
+            url
+            review_count
+            is_closed
+        }
+        GRAPHQL
+    
+
     def self.location_restaurants(location)
-        @search_url = "https://api.yelp.com/v3/businesses/search?location=#{location}&category=restaurants&limit=20&sort_by=best_match"
-        HTTParty.get(@search_url, @@headers)
+        responce = HTTParty.post(
+            @@url,
+            headers: {
+                "Authorization" => "Bearer #{@@api_key}",
+                "content-type" => "application/graphql"
+            },
+            body: <<-GRAPHQL
+            {
+                search(location: "#{location}", categories: "restaurants", limit: 20, sort_by: "best_match") {
+                    #{@@fragment}
+                }
+            }
+            GRAPHQL
+        )
     end
 
     def self.katchup_restaurants(location, date="", offset)
